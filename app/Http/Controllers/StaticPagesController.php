bef967b4e1d25346cc6b696a3a81654165bce978
vagrant <vagrant@homestead>
2018-08-17 18:29:28 +0000

    Finish static pages

---
+++ app/Http/Controllers/StaticPagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers; //命名空间在PHP5.3之后的语言特性；利用命名空间区分归类不同的代码功能，避免引起变量名或函数名的冲突 可以理解为文件路径。变量名理解为文件。
+
+use Illuminate\Http\Request; //use引用类 引用后可调用
+
+class StaticPagesController extends Controller //可以引用除private方法以外的其他方法
+{
+    public function home()
+    {
+        return view('static_pages/home');
+    }
+
+    public function help()
+    {
+        return view('static_pages/help');
+    }
+
+    public function about()
+    {
+        return view('static_pages/about');
+    }
+}
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title', 'Sample') - by SIYU</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
