97b241cd193cadac6e2843f2aefabc838288134d
vagrant <vagrant@homestead>
2018-08-18 07:48:37 +0000

    Create User Table

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,13 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
+     * 过滤用户提交的字段，只有在fillable中的字段才能被正常更新
      */
     protected $fillable = [
         'name', 'email', 'password',
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -10,26 +10,31 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
+     * 运行迁移时调用
      */
     public function up()
     {
+        /* 创建users表 */
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            // 定义数据表字段
+            $table->increments('id');           // 自增长id
+            $table->string('name');             // 用户名
+            $table->string('email')->unique();  // 邮箱 唯一值
+            $table->string('password');         // 密码 最大长度60
+            $table->rememberToken();            // 记住我相关信息
+            $table->timestamps();               //created_at创建时间 updated_at更新时间
         });
     }
 
     /**
      * Reverse the migrations.
+     * 回滚迁移时调用
      *
      * @return void
      */
     public function down()
     {
+        /* 删除users表 */
         Schema::dropIfExists('users');
     }
 }
